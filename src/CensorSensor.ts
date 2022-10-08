5605db6395750e6c96307d220cae968a3311c5c7
asfandalavi75 <89604405+asfandalavi75@users.noreply.github.com>
2022-10-08 16:58:50 -0400

    test 1

--- src/CensorSensor.ts
+++ src/CensorSensor.ts
@@ -118,7 +118,10 @@ export class CensorSensor {
       
       const tier = dict[dictWord];
 
-      if(phrase.indexOf(dictWord) !== -1 && this.enabledTiers[tier]) foundProfanity.push(dictWord);
+      if(phrase.indexOf(dictWord) !== -1 && this.enabledTiers[tier]){
+        foundProfanity.push(dictWord);
+        foundProfanity.push(tier);
+      }
     });
 
     return foundProfanity;
